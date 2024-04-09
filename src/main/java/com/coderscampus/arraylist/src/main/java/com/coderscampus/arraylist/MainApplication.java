9140969cf23df69a173520f39779fbb21dc279c8
sarahalmakki <149714925+sarahalmakki@users.noreply.github.com>
2024-04-09 09:19:14 -0500

    Update my work

---
+++ src/main/java/com/coderscampus/arraylist/src/main/java/com/coderscampus/arraylist/MainApplication.java
@@ -0,0 +1,23 @@
+package com.coderscampus.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		
+
+CustomList<String> elements = new CustomArrayList<>();         
+elements.add("Apple");
+elements.add("Orange");
+elements.add("Lime");
+elements.add("Blackberry");
+elements.add("Kiwi");
+elements.add("Strawberry");
+elements.add("Granberry");
+elements.add("Grape");
+elements.add("Banana");
+	}
+
+}
